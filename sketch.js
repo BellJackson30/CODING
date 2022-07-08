1923d71fcd758bcb3053f2a867563f06d2ae9211
BellJackson30 <108763454+BellJackson30@users.noreply.github.com>
2022-07-07 22:52:51 -0400

    Add files via upload
    Re-edited sketch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
+let xPos;
+let yPos;
+let ySpeed=5
+let xSpeed=5
+let santa;
+let mouseDist
+let score=0
+function preload(){
+
+
+santa=loadImage('santaclause.js')
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
   background(0,148,255);
   rectMode(CENTER);
+
+  createCanvas(windowWidth, windowHeight)
+xPos= random(windowWidth)
+yPos= random(windowHeight)
+
+textAlign(CENTER)
+
+
+
+
+
 }
 function draw(){
 fill(36,110,33)
@@ -24,6 +48,56 @@ rect(0,0,90,45)
  line(340,30, 270,40 )
   line(352,50, 270, 180)
   line(390,70, 270, 180)
-  createImg(https://www.istockphoto.com/photos/jet-plane-cartoon)
-    Position(20,200)
+
+  fill(255)
+  textSize(50)
+  text("Your Score Is " + score, windowWidth/2, 50)
+  mouseDist=dist(mouseX, mouseY, xPos, yPos)
+  print(mouseDist)
+
+
+
+    image(santa ,xPos,yPos,30,30)
+
+    xPos=xPos +xSpeed
+    yPos=yPos+ySpeed
+
+    if(xPos>= windowWidth-15 || xPos<=0){
+  xSpeed=xSpeed *-1;
+    }
+
+    if(yPos>=windowHeight-15 || yPos<=0){
+
+      ySpeed=ySpeed*-1
+    }
+  if(mouseDist<=15){
+    xPos=random(15,windowWidth-15)
+    yPos=random(15, windowHeight-15)
+  score++
+  xSpeed=xSpeed*4
+  ySpeed=ySpeed*4
+  }
+
+
+
+}
+function lineBrush(){
+  if(mouseIsPressed){
+    strokeWeight(5)
+    stroke(random(200),random(200), random(200))
+
+    line(pmouseX,pmouseY, mouseX, mouseY)
+  }
+
+
+
+
+
+
+
+  function windowResized(){
+
+  resizeCanvas(windowWidth,windowHeight)
+
+  }
 }
